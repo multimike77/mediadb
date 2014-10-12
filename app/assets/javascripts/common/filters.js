93a4afcf154714a171904af28bc0bfa132f92ae1
multimike <multimike77@gmail.com>
2014-10-12 12:46:41 +0200

    add filesize filter

--- app/assets/javascripts/common/filters.js
+++ app/assets/javascripts/common/filters.js
@@ -1,20 +1,43 @@
 /** Common filters. */
 define(['angular'], function(angular) {
-  'use strict';
+    'use strict';
 
-  var mod = angular.module('common.filters', []);
-  /**
-   * Extracts a given property from the value it is applied to.
-   * {{{
-   * (user | property:'name')
-   * }}}
-   */
-  mod.filter('property', function(value, property) {
-    if (angular.isObject(value)) {
-      if (value.hasOwnProperty(property)) {
-        return value[property];
-      }
-    }
-  });
-  return mod;
+    var mod = angular.module('common.filters', []);
+    /**
+     * Extracts a given property from the value it is applied to.
+     * {{{
+     * (user | property:'name')
+     * }}}
+     */
+    mod.filter('property', function(value, property) {
+        if (angular.isObject(value)) {
+            if (value.hasOwnProperty(property)) {
+                return value[property];
+            }
+        }
+    });
+
+    /**
+     * Formats a number as file size
+     */
+    mod.filter('fileSize', function() {
+        return function(bytes, precision) {
+            if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) {
+                return '-';
+            }
+            if (bytes === 0) {
+                return '0 bytes';
+            }
+            if (typeof precision === 'undefined') {
+                precision = 1;
+            }
+
+            var units = ['bytes', 'KB', 'MB', 'GB', 'TB', 'PB'],
+                number = Math.floor(Math.log(bytes) / Math.log(1024));
+
+            return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) + ' ' + units[number];
+        };
+    });
+
+    return mod;
 });
